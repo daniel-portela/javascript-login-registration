1cc45d6cf0fd8b00917e673da23309040d407c03
Daniel Portela <danielportela.ti@gmail.com>
2024-02-11 16:37:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+<div align="center">
+
+Projeto: Sistema de registro e login com validação JavaScript
+
+![HTML](https://img.shields.io/badge/-HTML-0D1117?style=for-the-badge&logo=html5&labelColor=0D1117)&nbsp;
+![CSS](https://img.shields.io/badge/-CSS-0D1117?style=for-the-badge&logo=CSS3&logoColor=blue&labelColor=0D1117)&nbsp;
+![JavaScript](https://img.shields.io/badge/-javascript-0D1117?style=for-the-badge&logo=javascript&logoColor=yellow&labelColor=0D1117)&nbsp;
+
+<p>Este projeto é um sistema de registro e login desenvolvido com tecnologias web, incluindo HTML, CSS e JavaScript. Seu objetivo principal é fornecer aos usuários uma maneira intuitiva e segura de criar uma conta e acessar o sistema posteriormente.</p>
+
+<a href="https://registrologin.netlify.app/"><strong>Demonstração ao vivo</strong></a>
+</div><br>
+
+![alt text](preview.jpg)
+
+
+![alt text](preview-error.jpg)
+
+![alt text](registro-login-responsivo.jpg)
+
+
+A página de registro apresenta campos para inserção de informações pessoais, como nome, sobrenome, endereço de e-mail, número de telefone e senha. Além disso, há validações implementadas para garantir que os dados inseridos sejam formatados corretamente e atendam aos requisitos de segurança, como o formato de e-mail válido e a complexidade da senha.
+
+<b>Desenvolvimento do projeto:</b>
+
+<b>HTML5:</b>
+
+Estrutura básica de um formulário de registro e login.
+Campos para inserção de informações como nome, sobrenome, endereço de e-mail, número de telefone e senha.
+Botões para enviar o formulário de registro e alternar para o formulário de login.
+
+<b>Estilos CSS3:</b>
+
+Estilização dos elementos HTML5 para proporcionar uma experiência visual agradável.
+Utilização de fontes e cores para destacar informações importantes.
+Responsividade para garantir uma boa aparência em diferentes dispositivos.
+
+<b>JavaScript:</b>
+
+Funções para validar os dados inseridos nos campos do formulário.
+Verificação de campos obrigatórios e formatos válidos para e-mail, número de telefone e senha.
+Feedback visual para indicar sucesso ou falha durante o processo de registro.
+Esse projeto oferece uma interface amigável e funcional para que os usuários possam registrar suas informações com segurança e facilidade, além de permitir o acesso rápido por meio do login.
+
+<br><b>Contato:</b>
+
+<p>Se você quiser entrar em contato comigo, sinta-se a vontade.</p> 
+
+<a href="https://linkedin.com/in/danielengineer" target="_blank">
+  <img align="center" src="https://img.shields.io/badge/ - LinkedIn-05122A?style=flat&logo=linkedin" alt="linkedin"/>
+</a>
+ <a href="https://instagram.com/danielengineer_" target="_blank">
+ <img align="center" src="https://img.shields.io/badge/ - Instagram-05122A?style=flat&logo=instagram" alt="instagram"/>
+</a>
+ <a href="https://wa.me/77999109489" target="_blank">
+ <img align="center" src="https://img.shields.io/badge/-Whatsapp-05122A?style=flat&logo=whatsapp" alt="whatsapp"/>
+</a>
+<a href="https://www.behance.net/danielengineer_" target="_blank">
+ <img align="center" src="https://img.shields.io/badge/-behance-05122A?style=flat&logo=behance" alt="behance"/>
+</a>
---
+++ assets/js/script.js
@@ -0,0 +1,126 @@
+const regsiterForm = document.querySelector('.register-form');
+const submitBtn = document.getElementById('submit-btn');
+const submissionStatus = document.querySelector('.submission-status');
+
+// input values 
+const firstName = document.getElementById('firstname'),
+    lastName = document.getElementById('lastname'),
+    emailAddr = document.getElementById('email'),
+    phoneNumber = document.getElementById('phonenumber'),
+    password = document.getElementById('password'),
+    confirmPassword = document.getElementById('confirm-password');
+
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let isValidForm = validateInputValues();
+    if (isValidForm) {
+        submissionStatus.classList.add('successMessage');
+        submissionStatus.textContent = "Registrado com sucesso!";
+    } else {
+        submissionStatus.classList.add('errorMessage');
+        submissionStatus.textContent = "Falha no registro!";
+    }
+
+    setTimeout(() => {
+        submissionStatus.classList.remove('errorMessage', 'successMessage');
+    }, 1500);
+});
+
+function validateInputValues() {
+    let inputValidationStatus = [];
+    if (validateName(firstName.value)) {
+        inputStatus(true, firstName);
+        inputValidationStatus[0] = true;
+    } else {
+        inputStatus(false, firstName);
+        inputValidationStatus[0] = false;
+    }
+
+    if (validateName(lastName.value)) {
+        inputStatus(true, lastName);
+        inputValidationStatus[1] = true;
+    } else {
+        inputStatus(false, lastName);
+        inputValidationStatus[1] = false;
+    }
+
+    if (validateEmail(emailAddr.value)) {
+        inputStatus(true, emailAddr);
+        inputValidationStatus[2] = true;
+    } else {
+        inputStatus(false, emailAddr);
+        inputValidationStatus[2] = false;
+    }
+
+    if (validatePhoneNo(phoneNumber.value)) {
+        inputStatus(true, phoneNumber);
+        inputValidationStatus[3] = true;
+    } else {
+        inputStatus(false, phoneNumber);
+        inputValidationStatus[3] = false;
+    }
+
+    if (validatePassword(password.value)) {
+        inputStatus(true, password);
+        inputValidationStatus[4] = true;
+    } else {
+        inputStatus(false, password);
+        inputValidationStatus[4] = false;
+    }
+
+    if (confirmPassword.value.trim() !== "" && validateConfirmPassword(password.value, confirmPassword.value)) {
+        inputStatus(true, confirmPassword);
+        inputValidationStatus[5] = true;
+    } else {
+        inputStatus(false, confirmPassword);
+        inputValidationStatus[5] = false;
+    }
+
+    return (inputValidationStatus.includes(false) ? false : true);
+}
+
+// Validar primeiro e último nome 
+function validateName(nameTxt) {
+    const nameRegex = /^[A-Za-z]+$/; // Nome ou sobrenome contendo apenas letras
+    return nameRegex.test(nameTxt);
+}
+
+// Validar endereço de e-mail
+function validateEmail(emailTxt) {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(emailTxt);
+}
+
+// Validar número de telefone
+function validatePhoneNo(phoneTxt) {
+    const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    return phoneRegex.test(phoneTxt);
+}
+
+// Validar senha
+function validatePassword(passwordTxt) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    // Mínimo de oito caracteres, pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial
+    return passwordRegex.test(passwordTxt);
+}
+
+// Validar confirmação de senha
+function validateConfirmPassword(passwordTxt, confirmPasswordTxt) {
+    return (passwordTxt == confirmPasswordTxt);
+}
+
+
+// Status de erro ou sucesso
+function inputStatus(status, input) {
+    let inputGroup = input.parentElement;
+    if (status) {
+        inputGroup.classList.add('success');
+    } else {
+        inputGroup.classList.add('error');
+    }
+
+    setTimeout(() => {
+        inputGroup.classList.remove('success', 'error');
+    }, 1500);
+}
